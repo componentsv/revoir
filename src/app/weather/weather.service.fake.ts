5623280ced7a5e5d5f412a567e1c00bcba64e50e
anonymoussc <50c5ac69@opayq.com>
2018-12-27 08:42:30 +0700

    New weather.service.fake.ts

---
+++ src/app/weather/weather.service.fake.ts
@@ -0,0 +1,18 @@
+import { Observable, of } from 'rxjs';
+import { ICurrentWeather } from '../interfaces';
+import { IWeatherService } from './weather.service';
+
+export class WeatherServiceFake implements IWeatherService {
+    private fakeWeather: ICurrentWeather = {
+        city: 'Bursa',
+        country: 'TR',
+        date: new Date(),
+        image: '',
+        temperature: 280.32,
+        description: 'light intensity drizzle',
+    }
+
+    public getCurrentWeather(city: string, country: string): Observable<ICurrentWeather> {
+        return of(this.fakeWeather)
+    }
+}
